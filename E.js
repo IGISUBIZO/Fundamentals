5429df224d3b1f61e2abb1ffaa0989e7dde9f32a
Maxime757519 <olgamaxime7@gmail.com>
2025-03-27 08:44:24 +0200

    message

---
+++ E.js
@@ -0,0 +1,5 @@
+let str = "JavaScript";
+
+console.log(str.codePointAt(0)); // Output: 74 (J)
+console.log(str.codePointAt(1)); // Output: 97 (a)
+console.log(str.codePointAt(4)); // Output: 115 (s)
